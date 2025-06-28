9438978d9909ee8cd9ebfdbc56fd4092d1222f13
TPNayana <159258888+TPNayana@users.noreply.github.com>
2025-06-28 23:30:39 +0530

    Add files via upload

---
+++ JavaHandsOnSolutions_Week1/FinancialForecasting/ForecastCalculator.java
@@ -0,0 +1,10 @@
+public class ForecastCalculator {
+
+    // Recursive method to calculate future value
+    public static double predictFutureValue(double currentValue, double growthRate, int years) {
+        if (years == 0) {
+            return currentValue;
+        }
+        return predictFutureValue(currentValue * (1 + growthRate), growthRate, years - 1);
+    }
+}
